71241cb187d89e5a1ccf4a6cb14dea633355eb67
Bawankar <swapnil.bawankar@accenture.com>
2024-02-15 22:54:22 +0530

    launch command added

--- execute.sh
+++ execute.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 #
+
+## Build Docker Image using dockerfile
+
 sudo sh -c 'docker images'
 sudo sh -c 'docker build --ulimit nofile=1024000:1024000 -t autojen:v1 .'
 sudo sh -c 'docker images'
 
+## Launch container
+
+sudo sh -c 'docker run -dit --name myos1 autojen:v1'
