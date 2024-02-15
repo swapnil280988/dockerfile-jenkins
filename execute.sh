a61c0ce49323d8135d2b543c8e54845650b8a776
Bawankar <swapnil.bawankar@accenture.com>
2024-02-15 22:49:42 +0530

    updated ulimit

--- execute.sh
+++ execute.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #
 sudo sh -c 'docker images'
-sudo sh -c 'docker build -t autojen:v1 .'
+sudo sh -c 'docker build --ulimit nofile=1024000:1024000 -t autojen:v1 .'
 sudo sh -c 'docker images'
 
