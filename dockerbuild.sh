2657a8bb265ddafe7a902594cad64c9b6019ed0f
Bawankar <swapnil.bawankar@accenture.com>
2024-02-20 18:50:23 +0530

    1st update

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 sudo sh -c "docker images"
-sudo sh -c "docker build -t --ulimit nofile=1024000:1024000 mycustom:v1 ."
+sudo sh -c "docker build --ulimit nofile=1024000:1024000 -t mycustom:v1 ."
 sudo sh -c "docker images"
 sudo sh -c "docker tag mycustom:v1 swapnilca2809/mycustom:v1"
 sudo sh -c "docker push swapnilca2809/mycustom:v1"
